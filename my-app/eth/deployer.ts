ae8fa672c711266ba156542b4e1cc56f1ae87ad3
MahendraSnap <mahendra.zulqarnaen@snapinnovations.io>
2023-08-22 14:37:01 +0700

    mockAggAdjust

--- my-app/eth/deployer.ts
+++ my-app/eth/deployer.ts
@@ -1,18 +1,14 @@
 const ethers = require('ethers');
 
-const Marketplace = require('./artifacts/Marketplace.json'); 
-const NFT = require('./artifacts/NFT.json');
-const NFTAirdrop = require('./artifacts/NFTAirdrop.json');
-const TicketQueue = require('./artifacts/TicketQueue.json');
+const MockAgregator = require('./artifacts/MockAggregator.json');
 
-
-export const deployNFT = async function (_name:string, _symbol:string, _contractURI:string)  {
+export const deployMockAgg = async function (_price:number)  {
     if (window.ethereum) {
         const provider = await new ethers.BrowserProvider(window.ethereum);
         const signer = await provider.getSigner();
         try {
-            const factory = await new ethers.ContractFactory(NFT.abi, NFT.bytecode, signer);
-            const contract = await factory.deploy(_name, _symbol, _contractURI);
+            const factory = await new ethers.ContractFactory(MockAgregator.abi, MockAgregator.bytecode, signer);
+            const contract = await factory.deploy(ethers.parseEther(_price.toString()));
             await contract.waitForDeployment();
             console.log('NFT Contract deployed at:', await contract.getAddress());
             return await contract.getAddress();
@@ -24,60 +20,4 @@ export const deployNFT = async function (_name:string, _symbol:string, _contract
     }
 }
 
-export const deployMarketplace = async function (_listPrice:number)  {
-    if (window.ethereum) {
-        const provider = await new ethers.BrowserProvider(window.ethereum);
-        const signer = await provider.getSigner();
-        
-        try {
-            const factory = await new ethers.ContractFactory(Marketplace.abi, Marketplace.bytecode, signer);
-            const contract = await factory.deploy(ethers.parseEther(_listPrice.toString()));
-            await contract.waitForDeployment();
-            console.log('Marketplace Contract deployed at:', await contract.getAddress());
-            return await contract.getAddress();
-        } catch (error) {
-            console.error('Error deploying Marketplace contract:', error);
-        }
-    } else {
-        console.error('Metamask not found or not connected.');
-    }
-}
-
-export const deployNFTAirdrop = async function (_name:string, _symbol:string, _contractURI:string)  {
-    if (window.ethereum) {
-        const provider = await new ethers.BrowserProvider(window.ethereum);
-        const signer = await provider.getSigner();
-        
-        try {
-            const factory = await new ethers.ContractFactory(NFTAirdrop.abi, NFTAirdrop.bytecode, signer);
-            const contract = await factory.deploy(_name, _symbol, _contractURI);
-            await contract.waitForDeployment();
-            console.log('NFTAirdrop Contract deployed at:', await contract.getAddress());
-            return await contract.getAddress();
-        } catch (error) {
-            console.error('Error deploying NFTAirdrop contract:', error);
-        }
-    } else {
-        console.error('Metamask not found or not connected.');
-    }
-}
-
-export const deployTicketQueue = async function ()  {
-    if (window.ethereum) {
-        const provider = await new ethers.BrowserProvider(window.ethereum);
-        const signer = await provider.getSigner();
-        
-        try {
-            const factory = new ethers.ContractFactory(TicketQueue.abi, TicketQueue.bytecode, signer);
-            const contract = await factory.deploy();
-            await contract.waitForDeployment();
-            console.log('TicketQueue Contract deployed at:', await contract.getAddress());
-            return await contract.getAddress();
-        } catch (error) {
-            console.error('Error deploying TicketQueue contract:', error);
-        }
-    } else {
-        console.error('Metamask not found or not connected.');
-    }
-}
 
